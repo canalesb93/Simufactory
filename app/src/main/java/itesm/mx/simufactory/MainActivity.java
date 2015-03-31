2f15af43bd312b7f85b8224fbca2ca098f94c094
Ricardo Canales <canalesb93@gmail.com>
2015-03-30 19:23:46 -0600

    Removes some commented code

--- app/src/main/java/itesm/mx/simufactory/MainActivity.java
+++ app/src/main/java/itesm/mx/simufactory/MainActivity.java
@@ -93,13 +93,7 @@ public class MainActivity extends ActionBarActivity {
 
                 if(name.getText().length() > 0){
                     Session mySession = new Session(name.getText().toString(), password.getText().toString());
-
-//                    Map<String, Session> users = new HashMap<String, Session>();
-//                    users.put("alanisawesome", alanisawesome);
-//                    users.put("gracehop", gracehop);
-
                     sessionsRef.push().setValue(mySession);
-
                     Toast.makeText(getApplicationContext(), "Session created.", Toast.LENGTH_LONG).show();
 
                 } else {
@@ -111,25 +105,5 @@ public class MainActivity extends ActionBarActivity {
         });
 
     }
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+
 }
