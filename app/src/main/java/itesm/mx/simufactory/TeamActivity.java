e4f96c22aeca2bc29bf1ab148e3d851133a04cc1
EnriqueM <enrique.marr@gmail.com>
2015-04-30 14:43:55 -0500

    summary design

--- app/src/main/java/itesm/mx/simufactory/TeamActivity.java
+++ app/src/main/java/itesm/mx/simufactory/TeamActivity.java
@@ -98,6 +98,8 @@ public class TeamActivity extends MasterActivity {
         operationLV.setOnItemClickListener(itemListener);
 
 
+
+
 //        Button b = (Button) findViewById(R.id.button);
 //        b.setText("start");
 //        b.setOnClickListener(new View.OnClickListener() {
@@ -115,6 +117,11 @@ public class TeamActivity extends MasterActivity {
 //                }
 //            }
 //        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(getApplicationContext(), "Can't go back while session is running", Toast.LENGTH_SHORT).show();
     }
 
 
