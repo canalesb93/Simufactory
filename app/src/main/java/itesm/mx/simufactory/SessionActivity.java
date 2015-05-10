aa0883a5ef67f6de130fc599bc1ca3cc4b88520a
Roel Castaño <roelcastanomoreno@gmail.com>
2015-05-09 21:07:35 -0500

    Fix 2

--- app/src/main/java/itesm/mx/simufactory/SessionActivity.java
+++ app/src/main/java/itesm/mx/simufactory/SessionActivity.java
@@ -202,9 +202,12 @@ public class SessionActivity extends ActionBarActivity {
                     budgetConfig.removeTextChangedListener(this);
 
                     String cleanString = s.toString().replaceAll("[$,.]", "");
-
-                    int parsed = Integer.parseInt(cleanString);
-//                    String formatted = format.format((parsed));
+                    int parsed;
+                    if (cleanString.equals("")){
+                        parsed = 0;
+                    } else {
+                        parsed = Integer.parseInt(cleanString);
+                    }
                     String formatted = String.format("%,d", parsed);
                     formatted = '$' + formatted;
                     current = formatted;
@@ -235,7 +238,12 @@ public class SessionActivity extends ActionBarActivity {
                     String cleanString = s.toString().replaceAll("[$,.]", "");
 
 
-                    int parsed = Integer.parseInt(cleanString);
+                    int parsed;
+                    if (cleanString.equals("")){
+                        parsed = 0;
+                    } else {
+                        parsed = Integer.parseInt(cleanString);
+                    }
                     String formatted = String.format("%,d", parsed);
                     formatted = '$' + formatted;
 
