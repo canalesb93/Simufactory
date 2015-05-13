45127b9198b4c8870dbb72fb3ecd2f5147883d20
Ricardo Canales <canalesb93@gmail.com>
2015-05-13 01:41:02 -0500

    Adds gesture detection on operation activity

--- app/src/main/java/itesm/mx/simufactory/OperationActivity.java
+++ app/src/main/java/itesm/mx/simufactory/OperationActivity.java
@@ -3,8 +3,10 @@ package itesm.mx.simufactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
@@ -48,6 +50,7 @@ public class OperationActivity extends ActionBarActivity {
 
     Toast myToast;
 
+    private GestureDetector gestureDetector;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,6 +58,8 @@ public class OperationActivity extends ActionBarActivity {
         setContentView(R.layout.activity_operation);
 
         //Views initializations
+        gestureDetector = new GestureDetector(getApplicationContext(), new SwipeGestureDetector());
+
         TextView titleTextView = (TextView) findViewById(R.id.operationNameTV);
         TextView timeTextView = (TextView) findViewById(R.id.tvTime);
 
@@ -222,11 +227,54 @@ public class OperationActivity extends ActionBarActivity {
 
     }
 
-    public void toastNoResource(){
-
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (gestureDetector.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
     }
 
-    public void toastNoMoney(){
+    private void onLeftSwipe() {}
+
+    private void onRightSwipe() {
+        finish();
+    }
+
+    // Private class for gestures
+    private class SwipeGestureDetector
+            extends GestureDetector.SimpleOnGestureListener {
+        // Swipe properties, you can change it to make the swipe
+        // longer or shorter and speed
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MAX_OFF_PATH = 200;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2,
+                               float velocityX, float velocityY) {
+            try {
+                float diffAbs = Math.abs(e1.getY() - e2.getY());
+                float diff = e1.getX() - e2.getX();
+
+                if (diffAbs > SWIPE_MAX_OFF_PATH)
+                    return false;
+
+                // Left swipe
+                if (diff > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    OperationActivity.this.onLeftSwipe();
+
+                    // Right swipe
+                } else if (-diff > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    OperationActivity.this.onRightSwipe();
+                }
+            } catch (Exception e) {
+                Log.e("YourActivity", "Error on gestures");
+            }
+            return false;
+        }
     }
 
     @Override
